a278e40e4f62ea5084ec0ad268124f7693087a97
James Farrington <jfarrin1@student02.cse.nd.edu>
2017-12-04 16:14:51 -0500

    task4

--- cleanData.py
+++ cleanData.py
@@ -73,17 +73,18 @@ PAA= []
 for line in fPAA.readlines():
         words = line.split("\t")
         PAA.append(paperAuthorAffiliation(words[0], words[1], words[2], words[4], words[5]))
-#Load in paperAuthorAffiliations
-fAuthor = open("./microsoft/Authors.txt", "r")
-Authors = []
-for line in fAuthor.readlines():
-        words = line.split("\t")
-        Authors.append(author(words[0], words[1]))
+#Load in Authors
+#fAuthor = open("./microsoft/Authors.txt", "r")
+#Authors = []
+#for line in fAuthor.readlines():
+#        words = line.split("\t")
+#        Authors.append(author(words[0], words[1]))
 print('finished loading files')
 
 
 #takes in the frequent item sets of size K (F) and generates the new potential frequent item sets of size K+1
 def generateCandidates(F, K):
+#	print('generating candidates', K)
 	C = []
 	keys = list(F.keys())
 #	print('keys: ', keys)
@@ -93,32 +94,31 @@ def generateCandidates(F, K):
 		valid = 1
 		if K >1:
 			s1 = s1[0] + s1[1]
-			s1  = tuple(set(s1))
+			s1  = tuple(sorted(set(s1)))
+#		if K == 2:
 #			print('set: ', s1)
 		if len(s1) != K+1 or s1 in C:
 			continue
 		subSets = combinations(s1, K)
 		for s2 in subSets:
-#			print('sub: ', s2)
+#			if K ==2:
+#				print('sub: ', s2)
 			if K == 1:
 				s2 = s2[0]
-			if s2 not in F:
-#				print(s2, ' not in keys')
+			else:
+				s2 = tuple(sorted(s2))
+			if set(s2).issubset(set(F.keys())):
+				print(s2, ' not in keys')
 				valid = 0
 				break
 		if valid:
 			C.append(s1)	
-
-	t=0
-	for e in C:
-#		print(e)
-		t +=1
-		if t>6:
-			break
 	return C
 #	for key, value in F.items():
 # takes in the potential frequent item sets and finds their support. Removes if support < minsup			
 def countCandidates(C, transactions, minsup):
+#	print('C: ',C)
+#	print('counting candidates')
 	counts = {}
 	for item in C:
 		for t in transactions:
@@ -128,10 +128,33 @@ def countCandidates(C, transactions, minsup):
 				else:
 					counts[item] = 1
 	F = {k:v for (k,v) in counts.items() if v >= minsup}
-#	print(F)
-	return F						
+#	print ('F: ', F)
+	for key, value in sorted(F.items(), key=operator.itemgetter(1), reverse=True):
+		print(key,value)
+		break
+	return F					
 
+def getF(transactions, K, minsup):
+	process = 0.0
+        counts = {}
+        for key, values  in transactions.items():
+#		print (key,values)
+		if len(values) < K+1:
+                        continue
+                superClass = combinations(values, K+1)
+                for s1 in superClass:
+			process += 1
+			s1 = tuple(sorted(s1))
+#			print(s1)
+                        if s1 in counts:
+                                counts[s1] += 1
 
+			else:
+				counts[s1] = 1
+	for key, value in sorted(counts.items(), key=operator.itemgetter(1), reverse=True):
+		print(key, value)
+#		print(process)
+		return {k:v for (k,v) in counts.items() if v >=minsup} 
 #Task 4 - Apiori
 def Apriori(minsup):
 	#create all transactions (1 per paper)
@@ -152,48 +175,34 @@ def Apriori(minsup):
 	K = 1
 	FIS = F		#all frequent item sets
 
-        #use this test data to see that it works
-        test = {}
-        test['0'] = ['81135B5C', '06CB40E7', '4ACF163B', '7F41DB28']
+#testing data (not being used)
+	test = {}
+        test['0'] = ['81135B5C', '06CB40E7', '4ACF163B', '7F41DB28', 'XXXXXXXX']
+        test['4'] = ['81135B5C', '06CB40E7', '4ACF163B', '7F41DB28', 'XXXXXXXX']
         test['1'] = ['81135B5C', '06CB40E7', '4ACF163B', '7F41DB28']
-        test['2'] = ['81135B5C', '06CB40E7']
+        test['2'] = ['81135B5C', 'XXXXXXXX', '06CB40E7']
         test['3'] = ['81135B5C',  '4ACF163B', '7F41DB28']
-
+#	transactions = test
 	F2 = {}
-	F2['81135B5C'] = 4 
-	F2['06CB40E7'] = 3
-	F2['4ACF163B'] = 3
-	F2['7F41DB28'] = 3
-# comment below 3 lines to use real data
-	F = F2
-	transactions = test
-	FIS = F2
+	F2['81135B5C'] = 5 
+	F2['06CB40E7'] = 4
+	F2['4ACF163B'] = 4
+	F2['7F41DB28'] = 4
+	F2['XXXXXXXX'] = 3
+#	F=F2
+#	FIS =F
 	#while we have frequent itemsets do...
 	while (len(F) != 0):
-		C = generateCandidates(F, K)
-		F = countCandidates(C, transactions, minsup)
+		#faster to generate F this way if |F| is large
+		if len(F) > 200:
+			F = getF(transactions, K, minsup)
+		else:
+			C = generateCandidates(F, K)
+			F = countCandidates(C, transactions, minsup)
+#		print (len(F))
 		FIS.update(F)
 		K += 1
-	#print out final list of frequent item sets and their counts
-	sorted_FIS = sorted(FIS.items(), key=operator.itemgetter(1), reverse=True)
-	frequentCounts = [0,0,0,0]
-	frequentAuthors = [(1,1),(1,1),(1,1),(1,1)]
-	finished = 0
-	for key, value in sorted_FIS:
-		i=0
-		if isinstance(key, tuple):
-			i = len(key)-1	
-		if value > frequentCounts[i]:
-			frequentCounts[i] = value
-			frequentAuthors[i] = key
-			finished+=1
-		if finished == 4:
-			break
-	for i in range(4):
-		print (frequentAuthors[i], frequentCounts[i])
-
 	return FIS
 
 #call Apriori with minsup = 2
-Apriori(2)
-
+Apriori(30)
